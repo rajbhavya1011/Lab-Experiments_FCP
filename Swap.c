68db726c09aa09e864fde859b47881c285b20495
rajbhavya1011 <rajbhavya1011@gmail.com>
2023-12-19 00:57:13 +0530

    upload

---
+++ Swap.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int swap_3rd(int a,int b)
+{
+int t=a;
+a=b;
+b=t;
+printf("a =%d, b=%d",a,b);
+}
+int swap(int a,int b)
+{
+a=a+b;
+b=a-b;
+a=a-b;
+printf("a =%d, b=%d",a,b);
+}
+int main()
+{
+    int n, a1, a2;
+    printf("Enter 1 to swap with 3rd variable and 2 to swap without 3rd variable");
+    scanf("%d",&n);
+    printf("Enter two numbers");
+    scanf("%d",&a1);
+    scanf("%d",&a2);
+    if(n==1) swap_3rd(a1, a2);
+    if(n==2) swap(a1,a2);
+
+}
